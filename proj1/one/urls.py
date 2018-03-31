bba1a6f056621b949a7bdc67428f71b8a25389f9
keshavaggarwal <keshavaggarwal1311@gmail.com>
2018-04-01 01:32:25 +0530

    corrected files

--- proj1/one/urls.py
+++ proj1/one/urls.py
@@ -4,7 +4,7 @@ from . import views
 
 urlpatterns = [
 
-    url(r'$', views.index, name = 'index'),
+    url(r'^$', views.index, name = 'index'),
     url(r'^post/(?P<pk>\d+)/$', views.post_detail, name='post_detail'),
     url(r'^post/new/$', views.post_new, name = 'post_new'),
 
--- proj1/one/views.py
+++ proj1/one/views.py
@@ -2,13 +2,17 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from .forms import PostForm
 from django.http import HttpResponseRedirect
+from django.shortcuts import get_object_or_404
+
+from .models import form
 # Create your views here.
 
 def index(request):
-    return render(request, 'index.html')
+    posts = form.objects.all()
+    return render(request, 'index.html',{'posts': posts})
 
 def post_detail(request, pk):
-    post = get_object_or_404(Post, pk=pk)
+    post = get_object_or_404(form, pk=pk)
     return render(request, 'post_detail.html', {'post': post})
 
 def post_new(request):
